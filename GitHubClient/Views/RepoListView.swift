f1be3c5a6f724a812683fd09ac2d5a3a8f9d6550
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-18 23:06:00 +0900

    add state property and implement dummy async method

--- GitHubClient/Views/RepoListView.swift
+++ GitHubClient/Views/RepoListView.swift
@@ -1,19 +1,30 @@
 import SwiftUI
 
 struct RepoListView: View {
-    private let mockRepos: [Repo] = [
-        .mock1, .mock2, .mock3, .mock4, .mock5
-    ]
+    @State private var mockRepos: [Repo] = []
     
     var body: some View {
         NavigationView {
-            List(mockRepos) { repo in
-                NavigationLink(
-                    destination: RepoDetailView(repo: repo)) {
-                    RepoRowView(repo: repo)
+            if mockRepos.isEmpty {
+                ProgressView("loading...")
+            } else {
+                List(mockRepos) { repo in
+                    NavigationLink(
+                        destination: RepoDetailView(repo: repo)) {
+                        RepoRowView(repo: repo)
+                    }
                 }
+                .navigationTitle("Repositories")
             }
-            .navigationTitle("Repositories")
+        }
+        .onAppear {
+            loadRepos()
+        }
+    }
+    
+    private func loadRepos() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            mockRepos = [ .mock1, .mock2, .mock3, .mock4, .mock5 ]
         }
     }
 }
