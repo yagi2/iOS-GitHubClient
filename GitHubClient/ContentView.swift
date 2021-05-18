b57fdecc8185eca2371bb6b8fa5a01eb3a2acb61
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-18 22:35:40 +0900

    add cell viww and logo asset

--- GitHubClient/ContentView.swift
+++ GitHubClient/ContentView.swift
@@ -2,8 +2,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Image("GitHubMark")
+                .resizable()
+                .frame(width: 44, height:44)
+            VStack(alignment: .leading) {
+                Text("Owner Name")
+                    .font(.caption)
+                Text("Repository Name")
+                    .font(.body)
+                    .fontWeight(.semibold)
+            }
+        }
     }
 }
 
