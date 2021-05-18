cab742eb38d3e0ecef0b575d9b619aeed8ce5371
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-18 23:02:44 +0900

    add repository detail view and file package

---
+++ GitHubClient/Views/RepoRowView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct RepoRowView: View {
+    let repo: Repo
+    var body: some View {
+        HStack {
+            Image("GitHubMark")
+                .resizable()
+                .frame(width: 44, height:44)
+            VStack(alignment: .leading) {
+                Text(repo.name)
+                    .font(.caption)
+                Text(repo.owner.name)
+                    .font(.body)
+                    .fontWeight(.semibold)
+            }
+        }
+    }
+}
+
+struct RepoRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepoRowView(repo: .mock1)
+    }
+}
