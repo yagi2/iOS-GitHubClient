5258c472d63e4e64b9c7fa8805b1468d77adcb56
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-18 22:25:10 +0900

    Initial Commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by Itsuki Aoyagi on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
