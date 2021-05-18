fe3c72427df7022a9dfcbbdf17d54c440247361e
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-18 23:59:55 +0900

    implement mock preview

--- GitHubClientTests/RepoListViewModelTest.swift
+++ GitHubClientTests/RepoListViewModelTest.swift
@@ -67,27 +67,4 @@ class RepoListViewModelTest: XCTestCase {
                 enforceOrder: true
             )
         }
-    
-    struct MockRepoRepository: RepoRepository {
-            let repos: [Repo]
-            let error: Error?
-
-            init(repos: [Repo], error: Error? = nil) {
-                self.repos = repos
-                self.error = error
-            }
-
-            func fetchRepos() -> AnyPublisher<[Repo], Error> {
-                if let error = error {
-                    return Fail(error: error)
-                        .eraseToAnyPublisher()
-                }
-
-                return Just(repos)
-                    .setFailureType(to: Error.self)
-                    .eraseToAnyPublisher()
-            }
-        }
-
-        struct DummyError: Error {}
 }
